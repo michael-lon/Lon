465778591cb28e1bd9756591cfbc0e2dfee63f9a
michael-lon <omarlaricoiquise31@gmail.com>
2025-04-11 12:22:17 -0500

    calculadora

--- CalcFX/src/main/java/pe/edu/upeu/calcfx/Application.java
+++ CalcFX/src/main/java/pe/edu/upeu/calcfx/Application.java
@@ -1,7 +1,6 @@
 package pe.edu.upeu.calcfx;
 
 public class Application {
-    public static void main(String[] args) {
-        CalcFxApplication.main(args);
+    public static void main(String[] args) {CalcFxApplication.main(args);
     }
 }
--- CalcFX/src/main/java/pe/edu/upeu/calcfx/control/CalcfxControl.java
+++ CalcFX/src/main/java/pe/edu/upeu/calcfx/control/CalcfxControl.java
@@ -1,8 +1,30 @@
 package pe.edu.upeu.calcfx.control;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CalcfxControl {
 
+    @FXML
+    private TextField txtResultado;
+
+    private void escribirNumero(String numero){
+        txtResultado.appendText(numero);
+    }
+
+    @FXML
+    private void controlClick(ActionEvent event){
+        Button boton = (Button) event.getSource();
+        switch (boton.getId()){
+            case "btn0","btn1","btn2","btn3","btn4","btn5","btn6","btn7","btn8","btn9":
+            {escribirNumero(boton.getText());
+            break;}
+
+            default: {} break;
+        }
+    }
 }
