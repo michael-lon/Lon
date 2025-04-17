4e93bb3713542d0fbde9876f11b3d60c3eba3af5
michael-lon <omarlaricoiquise31@gmail.com>
2025-04-17 18:59:58 -0500

    avance 15/04

--- CalcFX/src/main/java/pe/edu/upeu/calcfx/control/CalcfxControl.java
+++ CalcFX/src/main/java/pe/edu/upeu/calcfx/control/CalcfxControl.java
@@ -15,16 +15,69 @@ public class CalcfxControl {
     private void escribirNumero(String numero){
         txtResultado.appendText(numero);
     }
+    private void agregarOperador(String operador){
 
+        if (txtResultado.getText().contains("+")){
+            txtResultado.setText("hola");
+        }else {
+            txtResultado.appendText(" "+ operador+" ");
+        }
+
+
+    }
+    private void calcularResultado() {
+        try {
+            String[] tokens = txtResultado.getText().split(" ");
+            if (tokens.length < 3) {
+                return;
+            }
+            double num1 = Double.parseDouble(tokens[0]);
+            String operador = tokens[1];
+            double num2 = Double.parseDouble(tokens[2]);
+            double resultado = 0;
+            switch (operador) {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0) {
+                        resultado = num1 / num2;
+                    } else {
+                        txtResultado.setText("Error: Div/0");
+                        return;
+                    }
+                    break;
+            }
+            txtResultado.setText(String.valueOf(resultado));
+        } catch (Exception e) {
+            txtResultado.setText("Error");
+            System.out.println(e.getMessage());
+        }
+    }
     @FXML
     private void controlClick(ActionEvent event){
         Button boton = (Button) event.getSource();
-        switch (boton.getId()){
-            case "btn0","btn1","btn2","btn3","btn4","btn5","btn6","btn7","btn8","btn9":
-            {escribirNumero(boton.getText());
+        switch (boton.getId()) {
+            case "btn0", "btn1", "btn2", "btn3", "btn4", "btn5", "btn6", "btn7", "btn8", "btn9": {
+                escribirNumero(boton.getText());
+                break;
+            }
+            case "btnsum", "btnres", "btnmul", "btndiv": {
+                agregarOperador(boton.getText());
+                break;
+            }
+            case "btnsup":{
+                txtResultado.setText(" ");
             break;}
-
-            default: {} break;
+            case "btnigual":{calcularResultado();
+            break;}
+            default: { break;}
         }
     }
 }
